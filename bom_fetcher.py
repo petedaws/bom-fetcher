271d2d5abc2aeda07a8bbd1e18382a60b005fd0e
Peter Dawson <petedaws@gmail.com>
2013-02-24 23:02:13 +1000

    added default user input

--- bom_fetcher.py
+++ bom_fetcher.py
@@ -7,8 +7,10 @@ mt_staplton_radar = '/products/IDR663.loop.shtml'
 img_regexp = r'(theImageNames\[\d\] = ")(/radar/)(.*\.png)".*'
 
 web_page = urllib2.urlopen(bom+mt_staplton_radar)
-
+directory = './'
+if len(sys.argv) > 1:
+	directory = sys.argv[1]
 for image in re.finditer(img_regexp,web_page.read()):
-    open(sys.argv[1]+image.group(3),'wb').write(urllib2.urlopen(bom+image.group(2)+image.group(3)).read())
+    open(directory+image.group(3),'wb').write(urllib2.urlopen(bom+image.group(2)+image.group(3)).read())
 
 
